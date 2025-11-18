35d28b423f435413f6ab1fa45030dbb25e40b597
nutmi1k <dotten7792@gmail.com>
2025-11-17 20:33:21 -0600

    first commit backend

---
+++ src/main/java/com/nutmi1k/movies/be/Application.java
@@ -0,0 +1,13 @@
+package com.nutmi1k.movies.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
